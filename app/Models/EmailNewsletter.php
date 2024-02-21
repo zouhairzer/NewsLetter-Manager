2c017dd06a0592aad87313fa299f82007bb93aea
KHALID-ZENNOUHI <khalidzennouhi08@gmail.com>
2024-02-21 14:04:56 +0100

    create all the table of the database

---
+++ app/Models/EmailNewsletter.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class EmailNewsletter extends Model
+{
+    use HasFactory;
+}
