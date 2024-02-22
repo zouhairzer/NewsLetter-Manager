c8ea628c5781764072e3ec79c8b69ee492f6258c
ait ouaguerd anass <aitougeurdanass@gmail.com>
2024-02-22 19:14:17 +0100

    fix the tamplet to back-office

---
+++ resources/views/partials/Auth/_navBare.blade.php
@@ -0,0 +1,51 @@
+
+<nav
+class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top text-white"
+>
+<div class="container">
+    <div class="navbar-wrapper">
+        <a class="navbar-brand" href="javascript:;">Login Page</a>
+    </div>
+    <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        aria-controls="navigation-index"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+    >
+        <span class="sr-only">Toggle navigation</span>
+        <span class="navbar-toggler-icon icon-bar"></span>
+        <span class="navbar-toggler-icon icon-bar"></span>
+        <span class="navbar-toggler-icon icon-bar"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a href="dashboard" class="nav-link">
+                    <i class="material-icons">dashboard</i>
+                    Dashboard
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="register" class="nav-link">
+                    <i class="material-icons">person_add</i>
+                    Register
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="login" class="nav-link">
+                    <i class="material-icons">fingerprint</i>
+                    Login
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="lock.html" class="nav-link">
+                    <i class="material-icons">lock_open</i>
+                    Lock
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
+</nav>
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,30 @@ Route::resources([
     'newsletters'=> NewsletterController::class,
     'categories'=> CategoryController::class,
 ]);
+
+// ---------- Alll views --------------- //
+Route::get('dashboard' , function(){
+return view('dashboard');
+});
+Route::get('/' , function(){
+return view('Auth.login');
+});
+Route::get('login' , function(){
+return view('Auth.login');
+});
+Route::get('register' , function(){
+return view('Auth.register');
+});
+Route::get('categorie' , function(){
+return view('tables.Categorie');
+});
+Route::get('emails' , function(){
+return view('tables.Emails');
+});
+Route::get('news_letter' , function(){
+return view('tables.News_Letter');
+});
+Route::get('users' , function(){
+return view('tables.user');
+});
+// --------------------------------------- //
